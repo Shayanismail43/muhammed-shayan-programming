c508258fcc0ca64c5ee2c853bb8845ceee871f4e
Shayanismail43 <shayanismail43@gmail.com>
2025-09-25 21:39:32 +0400

    Update factorial_userdefined.cpp

--- c++/basic logic/factorial_userdefined.cpp
+++ c++/basic logic/factorial_userdefined.cpp
@@ -1,1 +1,21 @@
+# include <iostream>
+using namespace std;
 
+int fact(int n)
+{
+  int i,f=1;
+  for (i=1;i<=n;i++)
+    {
+      f=f*i;
+    }
+  return f;
+}
+int main()
+{
+  int n,result;
+  cout<<"input the number";
+  cin>>n;
+  result=fact(n);
+cout<<"factorial of"<<num<<"is"<<result;
+return 0;
+}
