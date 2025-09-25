885d0656b6d9110e9cf4464f95864ec92b07a4a6
Shayanismail43 <shayanismail43@gmail.com>
2025-09-25 20:56:04 +0400

    Update display_the_days_of_week.cpp

--- display_the_days_of_week.cpp
+++ display_the_days_of_week.cpp
@@ -7,6 +7,18 @@ cout<<"enter a digit to display the days of week";
 cin>>n;
 switch(n)
 {
+case 1: cout<<"monday"; break;
+case 2: cout<<"tuesday"; break;
+case 3: cout<<"wednesday"; break;
+case 4: cout<<"thursday"; break;
+case 5: cout<<"friday"; break;
+case 6: cout<<"saturday"; break;
+case 7: cout<<"friday"; break;
+default:cout<<"invalid input";
+}
+return 0;
+}
+
 
 
 
