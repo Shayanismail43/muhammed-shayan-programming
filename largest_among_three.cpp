3e316200f4a534c392319c484a244c9dc4331d5e
Shayanismail43 <shayanismail43@gmail.com>
2025-09-25 20:44:58 +0400

    Create largest_among_three.cpp

---
+++ largest_among_three.cpp
@@ -0,0 +1,15 @@
+# include < iostream >
+using namespace std ;
+int main () 
+{
+int a ,b , c ;
+cout<<" Enter three numbers : ";
+cin>>a>>b>>c;
+if(a>b && a>c)
+cout<<" Largest is : "<<a;
+else if(b > c)
+cout<<" Largest is : "<<b;
+else
+cout<<" Largest is : "<<c;
+return 0; 
+}
