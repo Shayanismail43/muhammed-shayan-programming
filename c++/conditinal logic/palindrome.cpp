de11a6ea092dfa2f850010718ede21f0958e8db2
Shayanismail43 <shayanismail43@gmail.com>
2025-09-25 21:51:23 +0400

    Update palindrome.cpp

--- c++/conditinal logic/palindrome.cpp
+++ c++/conditinal logic/palindrome.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n, num, dig,rev=0;
+cout<<"enter a positive number";
+cin>>num;
+n=num;
+while (num>0)
+{
+  digit=num%10;
+  rev=(rev*10)+digit;
+  num=num/10;
+}
+if (n==rev)
+  cout<<"the number is a palindrome";
+else
+  cout<<"the number is not a palindrome";
+return 0;
+}
